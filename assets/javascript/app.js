798f465a35734239af611c013ca73fcdc56d0051
Donald P Polansky <zizuar@comcast.net>
2018-12-23 23:57:56 -0600

    Temporary file upload while system restores from malware infection.

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// * You'll create a trivia form with multiple choice or true/false options (your choice).
+
+// * The player will have a limited amount of time to finish the quiz. 
+
+//   * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+// * Don't let the player pick more than one answer per question.
+
+// * Don't forget to include a countdown timer.
+
+// ### Reminder: Submission on BCS
+
+// * Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
